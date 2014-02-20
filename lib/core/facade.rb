4c0eff19ce89bb12ee8ab334f3cb7dc38e11a835
Adrian Webb <adrian.webb@coraltech.net>
2014-02-20 05:01:38 -0500

    Merging the nucleon core interface with the core facade and renaming the interface utility class to console.

--- lib/core/facade.rb
+++ lib/core/facade.rb
@@ -1,126 +1,248 @@
 
 module Nucleon
+ 
+  VERSION = File.read(File.join(File.dirname(__FILE__), '..', '..', 'VERSION'))
+  
+  #-----------------------------------------------------------------------------
+  
+  def self.ui
+    Core.ui
+  end
+  
+  #---
+  
+  def self.logger
+    Core.logger
+  end
+  
+  #---
+  
+  def self.add_log_levels(*levels)
+    Util::Interface.add_log_levels(levels)  
+  end
+  
+  def self.log_level
+    Util::Interface.log_level
+  end
+  
+  def self.log_level=log_level
+    Util::Interface.log_level = log_level
+  end
+  
+  #-----------------------------------------------------------------------------
+  
+  def self.admin?
+    is_admin  = ( ENV['USER'] == 'root' )
+    ext_admin = exec(:check_admin) do |op, results|
+      if op == :reduce
+        results.values.include?(true)
+      else
+        results ? true : false
+      end
+    end
+    is_admin || ext_admin ? true : false
+  end
+  
+  #-----------------------------------------------------------------------------
+  # Status codes
+  
+  @@codes = Codes.new
+  
+  def self.code
+    @@codes
+  end
+  
+  def self.codes(*codes)
+    Codes.codes(*codes)
+  end
+
+  #-----------------------------------------------------------------------------
+  # Initialization
+  
+  def self.initialize
+    current_time = Time.now
     
-  #-----------------------------------------------------------------------------
-  # Core plugin type facade
-  
-  def self.extension(provider)
-    plugin(:extension, provider, {})
-  end
-  
-  #---
-  
-  def self.configuration(options, provider = nil)
-    plugin(:configuration, provider, options)
-  end
-  
-  def self.configurations(data, build_hash = false, keep_array = false)
-    plugins(:configuration, data, build_hash, keep_array)
-  end
-  
-  #---
-  
-  def self.action(provider, options)
-    plugin(:action, provider, options)
-  end
-  
-  def self.action_config(provider)
-    action(provider, { :settings => {}, :quiet => true }).configure
-  end
-  
-  def self.actions(data, build_hash = false, keep_array = false)
-    plugins(:action, data, build_hash, keep_array)  
-  end
-  
-  def self.action_run(provider, options = {}, quiet = true)
-    Plugin::Action.exec(provider, options, quiet)
-  end
-  
-  def self.action_cli(provider, args = [], quiet = false)
-    Plugin::Action.exec_cli(provider, args, quiet)
-  end
-  
-  #---
-  
-  def self.project(options, provider = nil)
-    plugin(:project, provider, options)
-  end
-  
-  def self.projects(data, build_hash = false, keep_array = false)
-    plugins(:project, data, build_hash, keep_array)
-  end
-   
-  #-----------------------------------------------------------------------------
-  # Cluster plugin type facade
-   
-  def self.network(name, options = {}, provider = nil)
-    plugin(:network, provider, Config.ensure(options).import({ :name => name }))
-  end
-  
-  def self.networks(data, build_hash = false, keep_array = false)
-    plugins(:network, data, build_hash, keep_array)
-  end
-   
-  #---
-  
-  def self.node(name, options = {}, provider = nil)
-    plugin(:node, provider, Config.ensure(options).import({ :name => name }))
-  end
-  
-  def self.nodes(data, build_hash = false, keep_array = false)
-    plugins(:node, data, build_hash, keep_array)
-  end
-  
-  #---
-  
-  def self.provisioner(options, provider = nil)
-    plugin(:provisioner, provider, options)
-  end
-  
-  #---
-  
-  def self.provisioners(data, build_hash = false, keep_array = false)
-    plugins(:provisioner, data, build_hash, keep_array)
-  end
-  
-  #---
-  
-  def self.command(options, provider = nil)
-    plugin(:command, provider, options)
-  end
-  
-  def self.commands(data, build_hash = false, keep_array = false)
-    plugins(:command, data, build_hash, keep_array)
-  end
+    Celluloid.logger = logger
+      
+    logger.info("Initializing the Nucleon plugin system at #{current_time}")
+    Config.set_property('time', current_time.to_i)
     
+    connection = Manager.connection
+      
+    connection.define_type :extension     => nil,         # Core
+                           :action        => :update,     # Core
+                           :project       => :git,        # Core
+                           :command       => :shell,      # Core
+                           :event         => :regex,      # Utility
+                           :template      => :json,       # Utility
+                           :translator    => :json        # Utility
+                           
+    connection.load_plugins(true)
+                                  
+    logger.info("Finished initializing Nucleon plugin system at #{Time.now}")    
+  end
+  
   #-----------------------------------------------------------------------------
-  # Utility plugin type facade
+  # Core plugin interface
   
-  def self.event(options, provider = nil)
-    plugin(:event, provider, options)
-  end
-  
-  def self.events(data, build_hash = false, keep_array = false)
-    plugins(:event, data, build_hash, keep_array)
+  def self.plugin_load(type, provider, options = {})
+    config = Config.ensure(options)
+    name   = config.get(:name, nil)
+    
+    logger.info("Fetching plugin #{type} provider #{provider} at #{Time.now}")
+    logger.debug("Plugin options: #{config.export.inspect}")
+    
+    connection = Manager.connection
+    
+    if name
+      logger.debug("Looking up existing instance of #{name}")
+      
+      existing_instance = connection.get(type, name)
+      logger.info("Using existing instance of #{type}, #{name}") if existing_instance
+    end
+    
+    return existing_instance if existing_instance
+    connection.create(type, provider, config.export)  
   end
   
   #---
   
-  def self.template(options, provider = nil)
-    plugin(:template, provider, options)
+  def self.plugin(type, provider, options = {})
+    default_provider = Manager.connection.type_default(type)
+    
+    if options.is_a?(Hash) || options.is_a?(Nucleon::Config)
+      config   = Config.ensure(options)
+      provider = config.get(:provider, provider)
+      options  = config.export
+    end
+    provider = default_provider unless provider # Sanity checking (see plugins)
+    
+    plugin_load(type, provider, options)
   end
   
-  def self.templates(data, build_hash = false, keep_array = false)
-    plugins(:template, data, build_hash, keep_array)
+  #---
+  
+  def self.plugins(type, data, build_hash = false, keep_array = false)
+    logger.info("Fetching multiple plugins of #{type} at #{Time.now}")
+    
+    group = ( build_hash ? {} : [] )
+    klass = class_const([ :nucleon, :plugin, type ])    
+    data  = klass.build_info(type, data) if klass.respond_to?(:build_info)
+    
+    logger.debug("Translated plugin data: #{data.inspect}")
+    
+    data.each do |options|
+      if plugin = plugin(type, options[:provider], options)
+        if build_hash
+          group[plugin.plugin_name] = plugin
+        else
+          group << plugin
+        end
+      end
+    end
+    return group.shift if ! build_hash && group.length == 1 && ! keep_array
+    group  
   end
+  
+  #---
+  
+  def self.get_plugin(type, name)
+    Manager.connection.get(type, name)
+  end
+  
+  #---
+  
+  def self.remove_plugin(plugin)
+    Manager.connection.remove(plugin)
+  end
+
+  #-----------------------------------------------------------------------------
+  # Plugin extensions
    
+  def self.exec(method, options = {})
+    Manager.connection.exec(method, options) do |op, data|
+      data = yield(op, data) if block_given?
+      data
+    end
+  end
+  
   #---
   
-  def self.translator(options, provider = nil)
-    plugin(:translator, provider, options)
+  def self.config(type, options = {})
+    Manager.connection.config(method, options)
   end
   
-  def self.translators(data, build_hash = false, keep_array = false)
-    plugins(:translator, data, build_hash, keep_array)
+  #---
+  
+  def self.check(method, options = {})
+    Manager.connection.check(method, options)
   end
+  
+  #---
+  
+  def self.value(method, value, options = {})
+    Manager.connection.value(method, value, options)
+  end
+       
+  #-----------------------------------------------------------------------------
+  # External execution
+   
+  def self.run
+    begin
+      logger.debug("Running contained process at #{Time.now}")
+      yield
+      
+    rescue Exception => error
+      logger.error("Nucleon run experienced an error! Details:")
+      logger.error(error.inspect)
+      logger.error(error.message)
+      logger.error(Util::Data.to_yaml(error.backtrace))
+  
+      ui.error(error.message) if error.message
+      raise
+    end
+  end
+  
+  #-----------------------------------------------------------------------------
+  # Utilities
+  
+  def self.class_name(name, separator = '::', want_array = FALSE)
+    components = []
+    
+    case name
+    when String, Symbol
+      components = name.to_s.split(separator)
+    when Array
+      components = name 
+    end
+    
+    components.collect! do |value|
+      value.to_s.strip.capitalize  
+    end
+    
+    if want_array
+      return components
+    end    
+    components.join(separator)
+  end
+  
+  #---
+  
+  def self.class_const(name, separator = '::')
+    components = class_name(name, separator, TRUE)
+    constant   = Object
+    
+    components.each do |component|
+      constant = constant.const_defined?(component) ? 
+                  constant.const_get(component) : 
+                  constant.const_missing(component)
+    end
+    constant
+  end
+  
+  #---
+  
+  def self.sha1(data)
+    Digest::SHA1.hexdigest(Util::Data.to_json(data, false))
+  end  
 end
