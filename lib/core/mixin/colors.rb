8e0eeba510e802eb44b28188704cd08609eafe60
Adrian Webb <adrian.webb@coraltech.net>
2014-03-19 23:58:32 -0400

    Adding a colors mixin that has some handy color text rendering methods.

---
+++ lib/core/mixin/colors.rb
@@ -0,0 +1,59 @@
+
+# Should be included via include
+#
+# include Mixin::Colors
+#
+
+module Nucleon
+module Mixin
+module Colors
+  
+  def black(string)
+    ::Nucleon::Util::Console.black(string)  
+  end
+  
+  #---
+  
+  def red(string)
+    ::Nucleon::Util::Console.red(string)   
+  end
+  
+  #---
+  
+  def green(string)
+    ::Nucleon::Util::Console.green(string)   
+  end
+  
+  #---
+  
+  def yellow(string)
+    ::Nucleon::Util::Console.yellow(string)   
+  end
+  
+  #---
+  
+  def blue(string)
+    ::Nucleon::Util::Console.blue(string)   
+  end
+  
+  #---
+  
+  def purple(string)
+    ::Nucleon::Util::Console.purple(string)   
+  end
+  
+  #---
+  
+  def cyan(string)
+    ::Nucleon::Util::Console.cyan(string)   
+  end
+  
+  #---
+  
+  def grey(string)
+    ::Nucleon::Util::Console.grey(string)   
+  end
+end
+end
+end
+
