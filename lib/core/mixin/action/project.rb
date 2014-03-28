792c52e52cb82213a550f64f098554aba6ab9c12
Adrian Webb <adrian.webb@coraltech.net>
2014-03-27 21:32:00 -0400

    Fixing some namespace issues and adding project creation options in the project action mixin.

--- lib/core/mixin/action/project.rb
+++ lib/core/mixin/action/project.rb
@@ -8,7 +8,7 @@ module Project
   # Settings
         
   def project_config
-    project_plugins = CORL.loaded_plugins(:project)
+    project_plugins = Nucleon.loaded_plugins(:project)
     
     register :project_provider, :str, :git, 'nucleon.core.mixin.action.project.options.project_provider' do |value|
       value = value.to_sym
@@ -21,7 +21,7 @@ module Project
     end
     register :project_reference, :str, nil, 'nucleon.core.mixin.action.project.options.project_reference' do |value|
       success = true
-      if info = CORL.plugin_class(:project).translate_reference(value)
+      if info = Nucleon.plugin_class(:project).translate_reference(value)
         if ! project_plugins.keys.include?(info[:provider].to_sym)
           warn('nucleon.core.mixin.action.project.errors.project_reference', { 
             :value     => value, 
@@ -35,26 +35,27 @@ module Project
       end
       success
     end
-    register :revision, :str, :master
+    register :project_revision, :str, :master, 'nucleon.core.mixin.action.project.options.project_revision'
   end
         
   #-----------------------------------------------------------------------------
   # Operations
         
-  def project_load(root_dir, update = false)
+  def project_load(root_dir, create = false, update = false)
         
     # 1. Set a default project provider (reference can override)
     # 2. Get project from root directory
-    # 3. Initialize project if not yet initialized
+    # 3. Initialize project if not yet initialized if requested
     # 4. Set remote if needed
     # 5. Checkout revision if needed
     # 6. Pull down updates if requested
           
     return Nucleon.project(extended_config(:project, {
+      :create    => create,
       :provider  => settings[:project_provider],
       :directory => root_dir,
-      :url       => settings[:reference],
-      :revision  => settings[:revision],
+      :url       => settings[:project_reference],
+      :revision  => settings[:project_revision],
       :pull      => update
     }))
   end       
