e881a24d311b16de4daace1761fb47a97c1473bf
Adrian Webb <adrian.webb@coraltech.net>
2014-04-12 07:00:00 -0400

    Ensuring the configuration collection is parallel capable and changing the save file name.

--- lib/core/config/collection.rb
+++ lib/core/config/collection.rb
@@ -6,50 +6,63 @@ class Collection
   #-----------------------------------------------------------------------------
   # Property accessor / modifiers
 
+  @@lock       = Mutex.new
   @@properties = {}
   
   #---
   
   def self.all
-    return @@properties
+    @@properties
   end
   
   #---
   
   def self.get(name)
-    return @@properties[name.to_sym]
+    value = nil
+    @@lock.synchronize do
+      value = @@properties[name.to_sym]
+    end
+    value
   end
   
   #---
   
   def self.set(name, value)
-    @@properties[name.to_sym] = value
+    @@lock.synchronize do
+      @@properties[name.to_sym] = value
+    end
   end
   
   #---
   
   def self.delete(name)
-    @@properties.delete(name.to_sym)
+    @@lock.synchronize do
+      @@properties.delete(name.to_sym)
+    end
   end
    
   #---
   
   def self.clear
-    @@properties = {}
+    @@lock.synchronize do
+      @@properties = {}
+    end
   end
   
   #---
   
   def self.save(options = {})
     unless Util::Data.empty?(options[:log_dir])
-      log_dir = options[:log_dir]
+      @@lock.synchronize do
+        log_dir = options[:log_dir]
       
-      if options[:config_store]
-        unless File.directory?(log_dir)
-          FileUtils.mkdir_p(log_dir)
+        if options[:config_store]
+          unless File.directory?(log_dir)
+            FileUtils.mkdir_p(log_dir)
+          end
+          Util::Disk.write(File.join(log_dir, "properties.json"), Util::Data.to_json(@@properties, true))
+          Util::Disk.write(File.join(log_dir, "properties.yaml"), Util::Data.to_yaml(Util::Data.string_map(@@properties)))
         end
-        Util::Disk.write(File.join(log_dir, "common.json"), Util::Data.to_json(@@properties, true))
-        Util::Disk.write(File.join(log_dir, "common.yaml"), Util::Data.to_yaml(Util::Data.string_map(@@properties)))
       end
     end
   end
