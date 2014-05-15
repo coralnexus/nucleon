111707480f86d6592365f4bdac00b6f0ad0ffef2
Adrian Webb <adrian.webb@coraltech.net>
2014-05-15 14:19:34 -0400

    Adding some Gem utility methods.

--- lib/core/gems.rb
+++ lib/core/gems.rb
@@ -32,18 +32,8 @@ module Gems
     if reset || Util::Data.empty?(@@gems)
       logger.info("Registering external gem defined Nucleon plugins at #{Time.now}")
       
-      if defined?(Gem) 
-        if ! defined?(Bundler) && Gem::Specification.respond_to?(:latest_specs)
-          logger.debug("Not using bundler")
-          Gem::Specification.latest_specs(true).each do |spec|
-            register_gem(spec)
-          end
-        else
-          logger.debug("Using bundler or Gem specification without latest_specs")
-          Gem.loaded_specs.each do |name, spec|
-            register_gem(spec)
-          end     
-        end
+      each_gem do |spec|
+        register_gem(spec)
       end
     end
     @@gems
@@ -78,6 +68,57 @@ module Gems
         @@gems[name][:namespaces] << namespace unless @@gems[name][:namespaces].include?(namespace)
       end
     end
+  end
+  
+  #-----------------------------------------------------------------------------
+  # Utilities
+  
+  def self.each_gem(&block)
+    if defined?(Gem) 
+      if ! defined?(Bundler) && Gem::Specification.respond_to?(:latest_specs)
+        logger.debug("Not using bundler")
+        Gem::Specification.latest_specs(true).each do |spec|
+          block.call(spec)
+        end
+      else
+        logger.debug("Using bundler or Gem specification without latest_specs")
+        Gem.loaded_specs.each do |name, spec|
+          block.call(spec)
+        end     
+      end
+    end
+  end
+  
+  #---
+  
+  def self.exist?(*names)
+    checks = Hash[names.map{|name| [ name.to_s, true ] }]
+    each_gem do |spec|
+      checks.delete(spec.name.to_s)
+    end
+    checks.empty? ? true : false
+  end
+  
+  #---
+  
+  def self.specs(*names)
+    checks = Hash[names.map{|name| [ name.to_s, true ] }]
+    specs  = []
+    
+    each_gem do |spec|
+      if checks.has_key?(spec.name.to_s)
+        specs << spec
+      end
+    end
+    specs
+  end
+  
+  #---
+  
+  def self.spec(name)
+    results = specs(name)
+    return results[0] unless results.empty?
+    nil
   end
 end
 end
