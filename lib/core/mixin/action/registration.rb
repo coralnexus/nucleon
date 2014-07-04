79d4889da4e6a7c190a82831ef9d3f130b97828a
Adrian Webb <adrian.webb@coraltech.net>
2014-07-04 18:03:20 -0400

    Adding translator registration methods in the registration action mixin.

--- lib/core/mixin/action/registration.rb
+++ lib/core/mixin/action/registration.rb
@@ -24,6 +24,26 @@ module Registration
       validate_plugins(:nucleon, :project, name, values)
     end
   end
+  
+  #---
+    
+  def register_translator(name, default = nil)
+    name = name.to_sym
+    
+    register name, :str, default do |value|
+      validate_plugins(:nucleon, :translator, name, value)
+    end
+  end
+  
+  #---
+    
+  def register_translators(name, default = nil)
+    name = name.to_sym
+    
+    register name, :array, default do |values|
+      validate_plugins(:nucleon, :translator, name, values)
+    end
+  end
 end
 end
 end
