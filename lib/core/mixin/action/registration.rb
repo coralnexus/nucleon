36539c2295f5a4f26b279704c0bf4cb30f21e8ac
Adrian Webb <adrian.webb@coraltech.net>
2014-07-09 23:03:03 -0400

    Adding register_file and register_directory methods to the action registration mixin.

--- lib/core/mixin/action/registration.rb
+++ lib/core/mixin/action/registration.rb
@@ -7,6 +7,26 @@ module Registration
   #-----------------------------------------------------------------------------
   # Options
     
+  def register_file(name, default = nil)
+    name = name.to_sym
+    
+    register name, :str, default do |value|
+      validate_file(value)
+    end
+  end
+  
+  #---
+    
+  def register_directory(name, default = nil)
+    name = name.to_sym
+    
+    register name, :str, default do |value|
+      validate_directory(value)
+    end
+  end
+  
+  #---
+    
   def register_project(name, default = nil)
     name = name.to_sym
     
@@ -43,6 +63,19 @@ module Registration
     register name, :array, default do |values|
       validate_plugins(:nucleon, :translator, name, values)
     end
+  end
+  
+  #-----------------------------------------------------------------------------
+  # Validators
+  
+  def validate_file(file_name)
+    file_name.nil? || File.exists?(file_name)  
+  end
+  
+  #---
+  
+  def validate_directory(dir_name)
+    dir_name.nil? || File.directory?(dir_name)
   end
 end
 end
