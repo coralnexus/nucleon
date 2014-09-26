1fcc7f5a6648b12fd53b4f519d3f8f547f2941e8
Adrian Webb <adrian.webb@coraltech.net>
2014-09-25 23:36:06 -0400

    More documentation updates.

--- lib/core/mixin/settings.rb
+++ lib/core/mixin/settings.rb
@@ -1,41 +1,35 @@
-
-# Should be included via include
-#
-# include Mixin::Settings
-#
-
 module Nucleon
 module Mixin
 module Settings
-  
+
   def settings(name)
     return get_hash([ :settings, name ])
   end
-    
+
   #---
 
   def set_settings(name, settings = {})
     return set([ :settings, name ], settings)
   end
-    
+
   #---
 
   def delete_settings(name)
     return delete([ :settings, name ])
   end
-    
+
   #---
-   
+
   def setting(name, key, default = '', format = false)
     return get([ :settings, name, key ], default, format)
   end
-     
+
   #---
 
   def set_setting(name, key, value = '')
     return set([ :settings, name, key ], value)
   end
-     
+
   #---
 
   def delete_setting(name, key)
