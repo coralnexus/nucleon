57f048847f284117d5a7ddd8fc34a9cac53838a1
Adrian Webb <adrian.webb@coraltech.net>
2014-10-26 23:31:07 -0400

    Adding code documentation to the plugin environment class.

--- lib/core/environment.rb
+++ lib/core/environment.rb
@@ -71,30 +71,122 @@ class Environment < Core
   #*****************************************************************************
   # Plugin type accessor / modifiers
 
+  # Return all of the defined namespaces in the plugin environment.
+  #
+  # * *Parameters*
+  #
+  # * *Returns*
+  #   - [Array<Symbol>]  Array of defined plugin namespaces
+  #
+  # * *Errors*
+  #
+  # See:
+  # - Nucleon::Config#get_hash
+  #
   def namespaces
     get_hash(:plugin_types).keys
   end
 
+  # Return all of the defined plugin types in a plugin namespace.
+  #
+  # * *Parameters*
+  #   - [String, Symbol] *namespace*  Namespace that contains plugin types
+  #
+  # * *Returns*
+  #   - [Array<Symbol>]  Array of defined plugin types
+  #
+  # * *Errors*
+  #
+  # See:
+  # - Nucleon::Config#get_hash
+  #
   def plugin_types(namespace)
     get_hash([ :plugin_types, namespace ]).keys
   end
 
+  # Define a new plugin type in a specified namespace.
+  #
+  # * *Parameters*
+  #   - [String, Symbol] *namespace*  Namespace that contains plugin types
+  #   - [String, Symbol] *plugin_type*  Plugin type name within namespace
+  #   - [String, Symbol] *default_provider*  Default provider (defaults to none)
+  #
+  # * *Returns*
+  #   - [Nucleon::Environment]  Returns reference to self for compound operations
+  #
+  # * *Errors*
+  #
+  # See:
+  # - Nucleon::Config#set
+  #
+  # See also:
+  # - #sanitize_id
+  #
   def define_plugin_type(namespace, plugin_type, default_provider = nil)
     set([ :plugin_types, namespace, sanitize_id(plugin_type) ], default_provider)
   end
 
+  # Define one or more new plugin types in a specified namespace.
+  #
+  # * *Parameters*
+  #   - [String, Symbol] *namespace*  Namespace that contains plugin types
+  #   - [Hash<String, Symbol|String, Symbol>] *type_info*  Plugin type, default provider pairs
+  #
+  # * *Returns*
+  #   - [Nucleon::Environment]  Returns reference to self for compound operations
+  #
+  # * *Errors*
+  #
+  # See:
+  # - #define_plugin_type
+  #
   def define_plugin_types(namespace, type_info)
     if type_info.is_a?(Hash)
       type_info.each do |plugin_type, default_provider|
         define_plugin_type(namespace, plugin_type, default_provider)
       end
     end
+    self
   end
 
+  # Check if a specified plugin type has been defined
+  #
+  # * *Parameters*
+  #   - [String, Symbol] *namespace*  Namespace that contains plugin types
+  #   - [String, Symbol] *plugin_type*  Plugin type name to check within namespace
+  #
+  # * *Returns*
+  #   - [Boolean]  Returns true if plugin type exists, false otherwise
+  #
+  # * *Errors*
+  #
+  # See:
+  # - Nucleon::Config#get_hash
+  #
+  # See also:
+  # - #sanitize_id
+  #
   def plugin_type_defined?(namespace, plugin_type)
     get_hash([ :plugin_types, namespace ]).has_key?(sanitize_id(plugin_type))
   end
 
+  # Return the default provider currently registered for a plugin type
+  #
+  # * *Parameters*
+  #   - [String, Symbol] *namespace*  Namespace that contains plugin types
+  #   - [String, Symbol] *plugin_type*  Plugin type name to fetch default provider
+  #
+  # * *Returns*
+  #   - [nil, Symbol]  Returns default provider if plugin type exists, nil otherwise
+  #
+  # * *Errors*
+  #
+  # See:
+  # - Nucleon::Config#get
+  #
+  # See also:
+  # - #sanitize_id
+  #
   def plugin_type_default(namespace, plugin_type)
     get([ :plugin_types, namespace, sanitize_id(plugin_type) ])
   end
@@ -103,7 +195,31 @@ class Environment < Core
   #*****************************************************************************
   # Loaded plugin accessor / modifiers
 
-  def define_plugin(namespace, plugin_type, base_path, file, &code)
+  # Define a new plugin provider of a specified plugin type.
+  #
+  # * *Parameters*
+  #   - [String, Symbol] *namespace*  Namespace that contains plugin types
+  #   - [String, Symbol] *plugin_type*  Plugin type name to fetch default provider
+  #   - [String] *base_path*  Base load path of the plugin provider
+  #   - [String] *file*  File that contains the provider definition
+  #
+  # * *Returns*
+  #   - [Nucleon::Environment]  Returns reference to self for compound operations
+  #
+  # * *Errors*
+  #
+  # * *Yields*
+  #   - [Hash<Symbol|ANY>] *data*  Plugin load information
+  #
+  # See:
+  # - Nucleon::Config#get_hash
+  # - Nucleon::Config#set
+  #
+  # See also:
+  # - #sanitize_id
+  # - #parse_plugin_info
+  #
+  def define_plugin(namespace, plugin_type, base_path, file, &code) # :yields: data
     namespace   = namespace.to_sym
     plugin_type = sanitize_id(plugin_type)
     plugin_info = parse_plugin_info(namespace, plugin_type, base_path, file)
@@ -122,12 +238,53 @@ class Environment < Core
 
       set([ :load_info, namespace, plugin_type, plugin_info[:provider] ], data)
     end
+    self
   end
 
+  # Return the load information for a specified plugin provider if it exists
+  #
+  # * *Parameters*
+  #   - [String, Symbol] *namespace*  Namespace that contains plugin types
+  #   - [String, Symbol] *plugin_type*  Plugin type name of provider
+  #   - [String, Symbol] *provider*  Plugin provider to return load information
+  #
+  # * *Returns*
+  #   - [nil, Hash<Symbol|ANY>]  Returns provider load information if provider exists, nil otherwise
+  #
+  # * *Errors*
+  #
+  # See:
+  # - Nucleon::Config#get
+  #
+  # See also:
+  # - #sanitize_id
+  #
   def loaded_plugin(namespace, plugin_type, provider)
     get([ :load_info, namespace, sanitize_id(plugin_type), sanitize_id(provider) ], nil)
   end
 
+  # Return the load information for namespaces, plugin types, providers if it exists
+  #
+  # * *Parameters*
+  #   - [nil, String, Symbol] *namespace*  Namespace to return load information
+  #   - [nil, String, Symbol] *plugin_type*  Plugin type name to return load information
+  #   - [nil, String, Symbol] *provider*  Plugin provider to return load information
+  #   - [ANY] *default*  Default results if nothing found (empty hash by default)
+  #
+  # * *Returns*
+  #   - [nil, Hash<Symbol|Symbol|Symbol|Symbol|ANY>]  Returns all load information if no parameters given
+  #   - [nil, Hash<Symbol|Symbol|Symbol|ANY>]  Returns namespace load information if only namespace given
+  #   - [nil, Hash<Symbol|Symbol|ANY>]  Returns plugin type load information if namespace and plugin type given
+  #   - [nil, Hash<Symbol|ANY>]  Returns provider load information if namespace, plugin type, and provider given
+  #
+  # * *Errors*
+  #
+  # See:
+  # - Nucleon::Config#get_hash
+  #
+  # See also:
+  # - #sanitize_id
+  #
   def loaded_plugins(namespace = nil, plugin_type = nil, provider = nil, default = {})
     load_info   = get_hash(:load_info)
 
@@ -152,10 +309,45 @@ class Environment < Core
     results
   end
 
+  # Check if a specified plugin type has been loaded
+  #
+  # * *Parameters*
+  #   - [String, Symbol] *namespace*  Namespace that contains plugin types
+  #   - [String, Symbol] *plugin_type*  Plugin type name to check
+  #
+  # * *Returns*
+  #   - [Boolean]  Returns true if plugin type has been loaded, false otherwise
+  #
+  # * *Errors*
+  #
+  # See:
+  # - Nucleon::Config#get_hash
+  #
+  # See also:
+  # - #sanitize_id
+  #
   def plugin_has_type?(namespace, plugin_type)
     get_hash([ :load_info, namespace ]).has_key?(sanitize_id(plugin_type))
   end
 
+  # Check if a specified plugin provider has been loaded
+  #
+  # * *Parameters*
+  #   - [String, Symbol] *namespace*  Namespace that contains plugin types
+  #   - [String, Symbol] *plugin_type*  Plugin type name to check
+  #   - [String, Symbol] *provider*  Plugin provider name to check
+  #
+  # * *Returns*
+  #   - [Boolean]  Returns true if plugin provider has been loaded, false otherwise
+  #
+  # * *Errors*
+  #
+  # See:
+  # - Nucleon::Config#get_hash
+  #
+  # See also:
+  # - #sanitize_id
+  #
   def plugin_has_provider?(namespace, plugin_type, provider)
     get_hash([ :load_info, namespace, sanitize_id(plugin_type) ]).has_key?(sanitize_id(provider))
   end
@@ -163,7 +355,38 @@ class Environment < Core
   #*****************************************************************************
   # Active plugin accessor / modifiers
 
-  def create_plugin(namespace, plugin_type, provider, options = {}, &code)
+  # Create a new plugin instance of a specified provider
+  #
+  # * *Parameters*
+  #   - [String, Symbol] *namespace*  Namespace that contains plugin types
+  #   - [String, Symbol] *plugin_type*  Plugin type name of provider
+  #   - [String, Symbol] *provider*  Plugin provider to return load information
+  #   - [Hash] *options*  Create options (plugin initialization configurations)
+  #
+  # * *Returns*
+  #   - [nil, Nucleon::Plugin::Base]  Returns plugin instance (inherited from Nucleon::Plugin::Base)
+  #
+  # * *Errors*
+  #
+  # * *Yields*
+  #   - [nil, Hash<Symbol|ANY>]  *type_info*  Provider load information if it has been loaded
+  #   - [Hash] *options*  Create options (plugin initialization configurations)
+  #
+  # See:
+  # - Nucleon::Plugin::Base
+  # - Nucleon::Config#get
+  # - Nucleon::Config#set
+  #
+  # See also:
+  # - #sanitize_id
+  # - #plugin_type_defined?
+  # - #loaded_plugin
+  # - Nucleon::Config
+  # - Nucleon::Config::array
+  # - Nucleon::Util::Data::subset
+  # - Nucleon::Facade#sha1
+  #
+  def create_plugin(namespace, plugin_type, provider, options = {}, &code) # :yields: type_info, options
     namespace   = namespace.to_sym
     plugin_type = sanitize_id(plugin_type)
     provider    = sanitize_id(provider)
@@ -195,6 +418,25 @@ class Environment < Core
     plugin
   end
 
+  # Return a plugin instance by name if it exists
+  #
+  # * *Parameters*
+  #   - [String, Symbol] *namespace*  Namespace that contains the plugin
+  #   - [String, Symbol] *plugin_type*  Plugin type name
+  #   - [String, Symbol] *plugin_name*  Plugin name to return
+  #
+  # * *Returns*
+  #   - [nil, Nucleon::Plugin::Base]  Returns a plugin instance of name specified if it exists
+  #
+  # * *Errors*
+  #
+  # See:
+  # - Nucleon::Plugin::Base
+  # - Nucleon::Config#get_hash
+  #
+  # See also:
+  # - #sanitize_id
+  #
   def get_plugin(namespace, plugin_type, plugin_name)
     namespace   = namespace.to_sym
     plugin_type = sanitize_id(plugin_type)
@@ -207,12 +449,55 @@ class Environment < Core
     nil
   end
 
-  def remove_plugin(namespace, plugin_type, instance_name, &code)
+  # Remove a plugin instance from the environment
+  #
+  # * *Parameters*
+  #   - [String, Symbol] *namespace*  Namespace that contains the plugin
+  #   - [String, Symbol] *plugin_type*  Plugin type name
+  #   - [String, Symbol] *instance_name*  Plugin instance name to tremove
+  #
+  # * *Returns*
+  #   - [nil, Nucleon::Plugin::Base]  Returns the plugin instance that was removed from environment
+  #
+  # * *Errors*
+  #
+  # * *Yields*
+  #   - [Nucleon::Plugin::Base] *plugin*  Plugin object being removed (cleanup)
+  #
+  # See:
+  # - Nucleon::Plugin::Base
+  # - Nucleon::Config#delete
+  #
+  # See also:
+  # - #sanitize_id
+  #
+  def remove_plugin(namespace, plugin_type, instance_name, &code) # :yields: plugin
     plugin = delete([ :active_info, namespace, sanitize_id(plugin_type), instance_name ])
     code.call(plugin) if code && plugin
     plugin
   end
 
+  # Return active plugins for namespaces, plugin types, providers if specified
+  #
+  # * *Parameters*
+  #   - [nil, String, Symbol] *namespace*  Namespace to return plugin instance
+  #   - [nil, String, Symbol] *plugin_type*  Plugin type name to return plugin instance
+  #   - [nil, String, Symbol] *provider*  Plugin provider to return plugin instance
+  #
+  # * *Returns*
+  #   - [nil, Hash<Symbol|Symbol|Symbol|Symbol|Nucleon::Plugin::Base>]  Returns all plugin instances if no parameters given
+  #   - [nil, Hash<Symbol|Symbol|Symbol|Nucleon::Plugin::Base>]  Returns namespace plugin instances if only namespace given
+  #   - [nil, Hash<Symbol|Symbol|Nucleon::Plugin::Base>]  Returns plugin type instances if namespace and plugin type given
+  #   - [nil, Hash<Symbol|Nucleon::Plugin::Base>]  Returns provider instances if namespace, plugin type, and provider given
+  #
+  # * *Errors*
+  #
+  # See:
+  # - Nucleon::Config#get_hash
+  #
+  # See also:
+  # - #sanitize_id
+  #
   def active_plugins(namespace = nil, plugin_type = nil, provider = nil)
     active_info = get_hash(:active_info)
 
@@ -243,7 +528,20 @@ class Environment < Core
   #*****************************************************************************
   # Utilities
 
-  def class_name(name, separator = '::', want_array = FALSE)
+  # Return a fully formed class name as a string
+  #
+  # * *Parameters*
+  #   - [String, Symbol, Array] *name*  Class name components
+  #   - [String, Symbol] *separator*  Class component separator (default '::')
+  #   - [Boolean] *want_array*  Whether or not to return array of final components or string version
+  #
+  # * *Returns*
+  #   - [String]  Returns fully rendered class name as string unless want_array is true
+  #   - [Array]  Returns array of final class components if want_array is true
+  #
+  # * *Errors*
+  #
+  def class_name(name, separator = '::', want_array = false)
     components = []
 
     case name
@@ -265,8 +563,20 @@ class Environment < Core
     components.join(separator)
   end
 
-  #---
-
+  # Return a fully formed class name as a machine usable constant
+  #
+  # * *Parameters*
+  #   - [String, Symbol, Array] *name*  Class name components
+  #   - [String, Symbol] *separator*  Class component separator (default '::')
+  #
+  # * *Returns*
+  #   - [Class Constant]  Returns class constant for fully formed class name of given components
+  #
+  # * *Errors*
+  #
+  # See also:
+  # - #class_name
+  #
   def class_const(name, separator = '::')
     components = class_name(name, separator, TRUE)
     constant   = Object
@@ -279,28 +589,72 @@ class Environment < Core
     constant
   end
 
-  #---
-
+  # Sanitize an identifier for internal plugin environment use.
+  #
+  # * *Parameters*
+  #   - [String, Symbol] *id_component*  Identifier to sanitize
+  #
+  # * *Returns*
+  #   - [Symbol]  Returns a sanitized symbol representing the given id component
+  #
+  # * *Errors*
+  #
   def sanitize_id(id_component)
     id_component.to_s.gsub(/([a-z0-9])(?:\-|\_)?([A-Z])/, '\1_\2').downcase.to_sym
   end
   protected :sanitize_id
 
-  #---
-
+  # Sanitize a class identifier for internal use.
+  #
+  # * *Parameters*
+  #   - [String, Symbol] *class_component*  Class identifier to sanitize
+  #
+  # * *Returns*
+  #   - [String]  Returns a sanitized string representing the given class component
+  #
+  # * *Errors*
+  #
   def sanitize_class(class_component)
     class_component.to_s.split('_').collect {|elem| elem.slice(0,1).capitalize + elem.slice(1..-1) }.join('')
   end
   protected :sanitize_class
 
-  #---
-
+  # Return a class constant representing a base plugin class generated from namespace and plugin_type.
+  #
+  # * *Parameters*
+  #   - [String, Symbol] *namespace*  Plugin namespace to constantize
+  #   - [String, Symbol] *plugin_type*  Plugin type to constantize
+  #
+  # * *Returns*
+  #   - [String]  Returns a class constant representing the plugin namespace and type
+  #
+  # * *Errors*
+  #
+  # See also:
+  # - #class_const
+  # - #sanitize_class
+  #
   def plugin_class(namespace, plugin_type)
     class_const([ sanitize_class(namespace), :plugin, sanitize_class(plugin_type) ])
   end
 
-  #---
-
+  # Parse plugin information for a specified namespace and plugin type.
+  #
+  # * *Parameters*
+  #   - [String, Symbol] *namespace*  Namespace that contains plugin types
+  #   - [String, Symbol] *plugin_type*  Plugin type name to fetch default provider
+  #   - [String] *base_path*  Base load path of the plugin provider
+  #   - [String] *file*  File that contains the provider definition
+  #
+  # * *Returns*
+  #   - [Hash<Symbol|ANY>]  Returns a hash of the parsed plugin information
+  #
+  # * *Errors*
+  #
+  # See also:
+  # - #sanitize_id
+  # - #sanitize_class
+  #
   def parse_plugin_info(namespace, plugin_type, base_path, file)
     dir_components   = base_path.split(File::SEPARATOR)
     file_components  = file.split(File::SEPARATOR)
