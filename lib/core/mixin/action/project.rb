398c1e9515e98bdbeeb5439f4c16b0888457ee06
Adrian Webb <adrian.webb@coraltech.net>
2015-01-19 15:32:55 -0500

    Adding the nucleon_resave flag to the project action mixin project initializer.

--- lib/core/mixin/action/project.rb
+++ lib/core/mixin/action/project.rb
@@ -3,49 +3,50 @@ module Nucleon
 module Mixin
 module Action
 module Project
-        
+
   #-----------------------------------------------------------------------------
   # Settings
-        
+
   def project_config
-    register_project_provider :project_provider, nil, [ 
-      'nucleon.core.mixin.action.project.options.project_provider', 
-      'nucleon.core.mixin.action.project.errors.project_provider' 
+    register_project_provider :project_provider, nil, [
+      'nucleon.core.mixin.action.project.options.project_provider',
+      'nucleon.core.mixin.action.project.errors.project_provider'
     ]
-    register_project :project_reference, nil, [ 
-      'nucleon.core.mixin.action.project.options.project_reference', 
-      'nucleon.core.mixin.action.project.errors.project_reference' 
+    register_project :project_reference, nil, [
+      'nucleon.core.mixin.action.project.options.project_reference',
+      'nucleon.core.mixin.action.project.errors.project_reference'
     ]
     register_str :project_revision, :master, 'nucleon.core.mixin.action.project.options.project_revision'
   end
-  
+
   #---
-  
+
   def project_ignore
     [ :project_provider, :project_reference, :project_revision ]
   end
-        
+
   #-----------------------------------------------------------------------------
   # Operations
-        
+
   def project_load(root_dir, create = false, update = false)
-        
+
     # 1. Set a default project provider (reference can override)
     # 2. Get project from root directory
     # 3. Initialize project if not yet initialized if requested
     # 4. Set remote if needed
     # 5. Checkout revision if needed
     # 6. Pull down updates if requested
-          
+
     return Nucleon.project(extended_config(:project, {
-      :create    => create,
-      :provider  => settings[:project_provider],
-      :directory => root_dir,
-      :url       => settings[:project_reference],
-      :revision  => settings[:project_revision],
-      :pull      => update
+      :create         => create,
+      :provider       => settings[:project_provider],
+      :directory      => root_dir,
+      :url            => settings[:project_reference],
+      :revision       => settings[:project_revision],
+      :pull           => update,
+      :nucleon_resave => true
     }))
-  end       
+  end
 end
 end
 end
