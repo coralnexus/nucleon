12621cd2865c5bf572d94f57fbf0b55dc050ef4f
Adrian Webb <adrian.webb@coraltech.net>
2015-09-05 14:35:37 -0400

    Fixing a localization string name in the commit action mixin.

--- lib/core/mixin/action/commit.rb
+++ lib/core/mixin/action/commit.rb
@@ -3,44 +3,44 @@ module Nucleon
 module Mixin
 module Action
 module Commit
-        
+
   #-----------------------------------------------------------------------------
   # Settings
-        
+
   def commit_config(optional = true)
-    
+
     if optional
       register_bool :commit, :false, 'nucleon.core.mixin.action.commit.options.commit'
     else
       settings[:commit] = true
     end
-    
+
     register_bool :allow_empty, true, 'nucleon.mixin.core.action.commit.options.allow_empty'
     register_bool :propogate_commit, false, 'nucleon.core.mixin.action.commit.options.propogate_commit'
-    
+
     register_str :message, '', 'nucleon.core.mixin.action.commit.options.message'
-    
+
     register_str :author, nil, 'nucleon.core.mixin.action.commit.options.author' do |value|
       if value.nil? || value.strip =~ /^[A-Za-z\s]+<\s*[^@]+@[^>]+\s*>$/
         next true
       end
-      warn('corl.core.mixin.action.commit.errors.author', { :value => value })
+      warn('nucleon.core.mixin.action.commit.errors.author', { :value => value })
       false
     end
   end
-  
+
   #---
-  
+
   def commit_ignore
     [ :commit, :allow_empty, :propogate_commit, :message, :author ]
   end
-        
+
   #-----------------------------------------------------------------------------
   # Operations
-        
+
   def commit(project, files = '.')
     success = true
-          
+
     if project && settings[:commit]
       success = project.commit(files, extended_config(:commit, {
         :allow_empty => settings[:allow_empty],
